be5d2e8ba931526954681d17f4f9ea82f86e102a
878123406 <878123406@qq.com>
2020-10-20 18:32:36 +0800

    spring boot依赖

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,11 +1,23 @@
 package test;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author hellofan
  */
+@Controller
+@SpringBootApplication
+@Configuration
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "hello world！";
     }
 }
